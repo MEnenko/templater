d9cd3f99a2ae39774d9ac594be1dac120b834b9c
Max Enenko <m.enenko@gmail.com>
2018-11-15 23:57:10 +0200

    stage 2

--- tamplater.js
+++ tamplater.js
@@ -1,19 +1,32 @@
 function Tamplater(){
+    var bankTag = [];
+    var bankTemlate = [];
     this.run = function() {
+        
+        for (var i = 0; i < bankTag.length; i++) {
+            var bootstrap = document.body.querySelector(bankTag[i]);
+            bootstrap.outerHTML = bankTemlate[i];
+        } 
+        /*
         document.body.style.background = '';
         var bootstrapButton = document.body.querySelector('bootstrap_button');
         bootstrapButton.remove()
-        
+        //bootstrapButton.outerHTML = '<p>Новый элемент!</p>';
         var botton = document.createElement('button');
         botton.setAttribute('class', 'btn btn-default');
         botton.setAttribute('type', 'submit');
         botton.innerHTML = "change button";
         document.body.append(botton);
+        */
     } 
+    this.addTag = function(tag, template){
+        bankTag.push(tag);
+        bankTemlate.push(template);
+    }
 
 }
 var tamplater = new Tamplater();
-var T = 'hellow';
+
 
             
         
