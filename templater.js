17c794b84d52622569d46b97eb49a9f60da6c1e5
Max Enenko <m.enenko@gmail.com>
2018-12-10 05:04:45 +0200

    refactoring templater.js

--- templater.js
+++ templater.js
@@ -1,76 +1,56 @@
-$.fn.templater = function(options) {
-    var tags = {};               
-    
-    $.extend(tags, options.tags);            
-   
-    for(var key in tags){                     
-        $(this.children()).each(function(){
-            $(this).replaceWith(render($(this) ,tags[key]))
-        });
-    }                
-}                                       
+(function() {
+	$.fn.templater = function(options) {
+		var tags = {};
+		$.extend(tags, options.tags);
+        var collection = $(this);        
+        while(isCheckCustomTags(collection, tags)) {            
+            for (var key in tags) {
+                collection.find(key).each(function() {
+                    var tag = $(this);
+                    tag.replaceWith(render(tag, tags[key]))
+                });
+            }
+        }
+    }      
 
-function render(tag, templat) {
-    var attributes = templat.match(/[^{}]+(?=})/g);
+	function render(tag, template) {
+        attributes = getAttributesTags(template);
+        if (attributes) {
+            attributes.forEach(function(attribute) {
+                var value;
+                var reg = getRegExp(attribute);
+                if (!template.includes(attribute)) {
+                    return;
+                }
+                if (attribute == 'html') {
+                    value = tag.html();
+                } else {
+                    value = tag.attr(attribute) || '';
+                }
+                template = getTemplateWithReplacedAttribute(template, reg, value);
+            });
+        }
+		return template;
+	}
 
-    attributes.forEach(function(element) {
-        var valueAttributes;
-
-        var reg = new RegExp('{{'+element+'}}');
-    
-        if (templat.includes(element) && element == 'html') {
-            valueAttributes = tag.text();
-            templat = templat.replace(reg, valueAttributes);
-        } else if(templat.includes(element)) {
-            valueAttributes = tag.attr(element);
-            templat = templat.replace(reg, valueAttributes);
-        } 
-    });        
-
-    return templat;
-}
-
-/*
-function Templater() {
-    
-    var tags = {};    
-    
-    this.run = function() {
-        for (var key in tags) {
-            var tagList = document.body.querySelectorAll(key);
-            for (tag of tagList) {
-                tag.outerHTML = render(tag, tags[key]);    
-            }            
-        } 
-    } 
-      
-    function render(tag, templat) {
-        var attributes = templat.match(/[^{}]+(?=})/g);
-
-        attributes.forEach(function(element) {
-            var valueAttributes;
-
-            var reg = new RegExp('{{'+element+'}}');
-           
-            if (templat.includes(element) && element == 'html') {
-                valueAttributes = tag.innerHTML;
-                templat = templat.replace(reg, valueAttributes);
-            } else if(templat.includes(element)) {
-                valueAttributes = tag.getAttribute(element);
-                templat = templat.replace(reg, valueAttributes);
-            } 
-        });        
-
-        return templat;
+	function getAttributesTags(template) {
+        return template.match(/[^{}]+(?=})/g);
     }
-    
-    this.addTag = function(tag, templat) {
-        tags[tag] = templat;        
+
+    function isCheckCustomTags(collection, tags) {
+        for (var tag in tags) {
+            if (collection.find(tag).length > 0) {
+                return true;
+            }
+        }
+        return false;
     }    
-}
 
-var templater = new Templater();
-*/
+	function getTemplateWithReplacedAttribute(template, reg, value) {
+		return template.replace(reg, value);
+	}
 
-            
-        
+	function getRegExp(attribute) {
+		return (new RegExp('{{' + attribute + '}}'));
+	}
+})()
